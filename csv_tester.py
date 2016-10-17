5da0168bf17ff0f4a38161224ad730567e493f22
ogal7 <ogallager@stuy.edu>
2016-10-17 01:01:20 -0400

    bringing booty back

--- csv_tester.py
+++ csv_tester.py
@@ -3,11 +3,27 @@ import csv
 fObj = open("peeps.csv") 
 d=csv.DictReader(fObj)
 
-for k in d:
-    print k
+
+
+def peeplez():
+	masterLis =[]
+	for k in d:
+		miniLis = [k['name'], k['age'], k['id']]
+		masterLis.append(miniLis)
+	return masterLis
+		#print k['name'], k['age'], k['id']
+
+
+def coursez():
+	masterLis =[]
+	for k in d:
+		miniLis = [k['code'], k['mark'], k['id']]
+		masterLis.append(miniLis)
+	return masterLis	
+	#print k['name'],",", k['id']
     #Q: What can you print here to make each line show only
     #   a name and its ID?
     #   eg,
     #   sasha, 3
 
-    
+
--- db_builder.py
+++ db_builder.py
@@ -3,26 +3,49 @@ import csv       #facilitates CSV I/O
 
 
 f="discobandit.db"
-
+#created = True# gives an error if you try to make the same table twice
 db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
+
+
 
 #==========================================================
 #INSERT YOUR POPULATE CODE IN THIS ZONE
 #...perhaps by beginning with these examples...
 
-'''
+#q = "CREATE TABLE students (name TEXT, id INTEGER)"
+#c.execute(q)    #run SQL query
+ # list of lists
+
+
+fObj = open("peeps.csv") 
+d=csv.DictReader(fObj)
 q = "CREATE TABLE students (name TEXT, id INTEGER)"
-
 c.execute(q)    #run SQL query
+for a in d:
+	command = "INSERT INTO students VALUES(" + a['name'] +"," + a['id']+ ")"
+	c.execute(command)
+
+	
+
+sObj = open("courses.csv")
+x = csv.DictReader(sObj)
+p = "CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER)"
+c.execute(p)
+for b in x:
+	command = "INSERT INTO courses VALUES("+ b['name'] +"," + b['mark']+"," + b['id']+")"
+	c.execute(command)
 
 
-q = "CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER)"
+	    #run SQL query
+	
 
-c.execute(q)
-'''
+#p = "CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER)"
+#c.execute(p)
+
 
 #==========================================================
+
 db.commit() #save changes
 db.close()  #close database
 
