95f4c22113b0a046fb2037d9bacdebd38c971923
ogal7 <ogallager@stuy.edu>
2016-10-20 01:32:05 -0400

    boiiiii

--- get_averages.py
+++ get_averages.py
@@ -1,26 +1,29 @@
 import sqlite3   #enable control of an sqlite database
-import csv       #facilitates CSV I/O
 
 
-f="discobandit.db"
-#created = True# gives an error if you try to make the same table twice
+
+f = "discobandit.db"
 db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 og = db.cursor() #da real og
 
 
+def getGrades():
+	command = "SELECT name, students.id, code, mark FROM students, courses WHERE students.id = courses.id"
+	og.execute(command)
+	finStr = ""
+	for item in og:
+		print item
+		finStr += item[0]
+		finStr += item[1]
+	return finStr	
 
-y = "SELECT name, students.id, mark FROM students, courses WHERE students.id = courses.id"
-gaga = og.execute(y)
-#print gaga
-#print "haha"
-for foo in gaga:
-	print foo[0]
+
+print(getGrades())
 
 
 
-
-#==========================================================
+# #==========================================================
 
 db.commit() #save changes
 db.close()  #close database
